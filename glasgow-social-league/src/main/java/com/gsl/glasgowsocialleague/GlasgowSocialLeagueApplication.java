fcf1abd8390ad98bc08601df3759ba495978fdb5
CSCairney <charlescairney@yahoo.co.uk>
2024-08-17 16:42:34 +0100

    initial structure build

---
+++ glasgow-social-league/src/main/java/com/gsl/glasgowsocialleague/GlasgowSocialLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.gsl.glasgowsocialleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlasgowSocialLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlasgowSocialLeagueApplication.class, args);
+    }
+
+}
---
+++ glasgow-social-league/src/test/java/com/gsl/glasgowsocialleague/GlasgowSocialLeagueApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gsl.glasgowsocialleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class GlasgowSocialLeagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

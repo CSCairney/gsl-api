7cc91780847a720c668bd93fbf3a75f1a66bd546
CSCairney <charlescairney@yahoo.co.uk>
2024-08-20 01:27:16 +0100

    test application properties

--- glasgow-social-league/src/test/java/com/gsl/glasgowsocialleague/GlasgowSocialLeagueApplicationTests.java
+++ glasgow-social-league/src/test/java/com/gsl/glasgowsocialleague/GlasgowSocialLeagueApplicationTests.java
@@ -2,8 +2,10 @@ package com.gsl.glasgowsocialleague;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Ensure the test profile is active
 class GlasgowSocialLeagueApplicationTests {
 
     @Test

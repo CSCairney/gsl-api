131545c569a832873f9c603dab0c7d6b6e4f4dfa
CSCairney <charlescairney@yahoo.co.uk>
2024-08-22 00:33:27 +0100

    Sport and service tests

--- glasgow-social-league/src/test/java/com/gsl/glasgowsocialleague/GlasgowSocialLeagueApplicationTests.java
+++ glasgow-social-league/src/test/java/com/gsl/glasgowsocialleague/GlasgowSocialLeagueApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")  // Ensure the test profile is active
+@ActiveProfiles("test")
 class GlasgowSocialLeagueApplicationTests {
 
     @Test

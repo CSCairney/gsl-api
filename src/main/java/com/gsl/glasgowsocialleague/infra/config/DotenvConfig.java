fa734d107f62f53f47285eaf451e457c734f9ab0
CSCairney <charlescairney@yahoo.co.uk>
2024-09-02 23:39:15 +0100

    main class routing fixed

--- src/main/java/com/gsl/glasgowsocialleague/infra/config/DotenvConfig.java
+++ src/main/java/com/gsl/glasgowsocialleague/infra/config/DotenvConfig.java
@@ -3,12 +3,20 @@ package com.gsl.glasgowsocialleague.infra.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @Configuration
 public class DotenvConfig {
 
     @Bean
-    public Dotenv dotenv() {
-        return Dotenv.configure().load();
+    public Dotenv dotenv(ConfigurableEnvironment env) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        dotenv.entries().forEach(entry -> {
+            // Add each .env entry to the Spring Environment
+            env.getSystemProperties().put(entry.getKey(), entry.getValue());
+        });
+
+        return dotenv;
     }
 }

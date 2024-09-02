155dda0708f94b8e0e9990d53bb3c5cfbafe37bb
CSCairney <charlescairney@yahoo.co.uk>
2024-09-02 22:20:46 +0100

    cors policys

--- src/main/java/com/gsl/glasgowsocialleague/infra/config/SecurityConfig.java
+++ src/main/java/com/gsl/glasgowsocialleague/infra/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://glasgow-social-league.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("https://glasgow-social-league.vercel.app/", "http://localhost:3000/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
